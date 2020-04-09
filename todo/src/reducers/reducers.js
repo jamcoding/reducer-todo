f90789089e32062183e1ce745c77938d4b5dcf93
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-04-08 19:45:09 -0500

    set up reducer

---
+++ todo/src/reducers/reducers.js
@@ -0,0 +1,12 @@
+export const initialObj = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+export const todoReducers = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
