da292b973928490e4c5d2402d80e5e1313b05640
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-04-08 21:40:12 -0500

    mvp completed

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -2,14 +2,42 @@ import React, { useState, useReducer } from 'react';
 import { todoReducers, initialObj } from '../reducers/todoReducers';
 
 const Todo = () => {
+    // console.log('props', props);
     const [todoState, dispatch] = useReducer(todoReducers, initialObj);
     const [newTodo, setNewTodo] = useState("");
 
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
+    const handleChanges = e => {
+        setNewTodo(e.target.value);
+    };
+
     return (
         <div className="container">
-            <ul className="todos">
-                <li className="todo">{todoState.item}</li>
-            </ul>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    className="todo-input" 
+                    type="text" 
+                    onChange={handleChanges} 
+                />
+                <button
+                    onClick={() => {
+                        dispatch({type: "ADD_TODO", payload: newTodo});
+                    }}>Add todo</button>
+                <button
+                    onClick={() => {
+                        dispatch({type: "CLEAR_COMPLETED"});
+                    }}>Clear completed todo</button>
+            </form>
+            {todoState.todos.map(todo => {
+                return <div key={todo.id}>
+                <p id={todo.id} className={`todo${todo.completed ? ' completed' : ''}`}
+                onClick={() => dispatch({ type: 'TOGGLE_COMPLETED', payload: todo.id }) }>
+                    {todo.item}</p>
+                </div>
+            })}
         </div>
     )
 }
--- todo/src/reducers/todoReducers.js
+++ todo/src/reducers/todoReducers.js
@@ -1,11 +1,55 @@
 export const initialObj = {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3892987589
+    todos: [
+        {
+            item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+        },
+        {
+            item: 'Clean my car',
+            completed: false,
+            id: 3949032382
+        },
+        {
+            item: 'Do laundry',
+            completed: false,
+            id: 39204589432
+        }
+    ]
 }
 
 export const todoReducers = (state, action) => {
     switch(action.type) {
+        case "ADD_TODO":
+            return {
+                ...state,
+                todos: [...state.todos,
+                    {
+                        item: action.payload,
+                        completed: false,
+                        id: Date.now()
+                    }
+                ]
+            }
+        case "TOGGLE_COMPLETED":
+            return {
+                ...state,
+                todos: state.todos.map(todo => {
+                    if(todo.id === action.payload) {
+                        return {
+                            ...todo,
+                            completed: !todo.completed
+                        }
+                    } else {
+                        return todo;
+                    }
+                })
+            }
+        case "CLEAR_COMPLETED": 
+            return {
+                ...state,
+                todos: state.todos.filter(todo => !todo.completed)
+            }
         default:
             return state;
     }
